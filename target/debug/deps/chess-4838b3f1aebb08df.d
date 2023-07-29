200d8c1d8a8432eb5877669f973061090958aa11
ElecAsymmetry <ebeyl@cox.net>
2023-07-28 21:52:17 -0500

    Created new repo

---
+++ target/debug/deps/chess-4838b3f1aebb08df.d
@@ -0,0 +1,5 @@
+/home/eb/rust/chess/target/debug/deps/chess-4838b3f1aebb08df.rmeta: src/main.rs
+
+/home/eb/rust/chess/target/debug/deps/chess-4838b3f1aebb08df.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/chess-f511706a035353a1.d
@@ -0,0 +1,5 @@
+/home/eb/rust/chess/target/debug/deps/chess-f511706a035353a1.rmeta: src/main.rs
+
+/home/eb/rust/chess/target/debug/deps/chess-f511706a035353a1.d: src/main.rs
+
+src/main.rs:
